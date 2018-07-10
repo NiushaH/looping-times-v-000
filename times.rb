433afa9c159462458bd386b94db2f9d4585bb5bb
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-10 19:43:21 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,13 +1,11 @@
-require 'pry'
-
 def using_times
 	#your code here
+	
 jewels_in_bag = 100
  
   3.times do 
     puts "Hiding 10 stolen jewels."
     jewels_in_bag = jewels_in_bag - 10
-  binding.pry
   end
 end
 
