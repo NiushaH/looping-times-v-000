400c5bb3a62da62f7da6edd37f4ae00090fa6574
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-10 19:51:45 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,11 +1,8 @@
 def using_times
 	#your code here
 	
-jewels_in_bag = 100
- 
-  3.times do 
-    puts "Hiding 10 stolen jewels."
-    jewels_in_bag = jewels_in_bag - 10
+  7.times do 
+    puts "Wingardium Leviosa"
   end
 end
 
